ec44baf4d96fedbd0bfdcdc288ef6aa9450c77ef
Ian DeJong <ianrdejong@gmail.com>
2021-12-28 01:00:21 -0800

    Update colour constants

--- js/colour-constants.js
+++ js/colour-constants.js
@@ -5,5 +5,27 @@ var BG_COL_SHADE_AMT = 10;
 var BG_COL_SHADE_1 = [BG_COL[0] - BG_COL_SHADE_AMT, BG_COL[1] - BG_COL_SHADE_AMT, BG_COL[2] - BG_COL_SHADE_AMT];
 var BG_COL_SHADE_2 = [BG_COL[0] - 2*BG_COL_SHADE_AMT, BG_COL[1] - 2*BG_COL_SHADE_AMT, BG_COL[2] - 2*BG_COL_SHADE_AMT];
 
+/*
+    Colour codes below are from https://www.w3schools.com/cssref/css_colors.asp
+*/
+
 // Basic
-var RED = [216, 41, 41];
+let RED = '#DD0000';
+let YELLOW = '#FFFF00';
+let ORANGE = '#FFA500';
+let TEAL = '#008080';
+let MAROON = '#800000';
+let PURPLE = '#800080';
+let GREEN = '#008000';
+let FUCHSIA = '#FF00FF';
+let BLUE = '#0000FF';
+let BLACK = '#000000';
+let WHITE = '#FFFFFF';
+
+// Basic alternate
+let RED_LIGHT = '#FF0000';
+
+// Special
+let ORANGERED = '#FF4500';
+let MIDNIGHTBLUE = '#191970';
+let LIMEGREEN = '#32CD32';
